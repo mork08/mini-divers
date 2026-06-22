0342418bce5bb478c05a8e28fbcbdba92abb0db0
Bunterninja2609 <121108968+Bunterninja2609@users.noreply.github.com>
2026-06-22 09:35:15 +0200

    Implemented Methods for Planet textures

---
+++ src/main/java/my_project/model/modes/planet/missions/ExtractionMission.java
@@ -0,0 +1,11 @@
+package my_project.model.modes.planet.missions;
+
+import KAGO_framework.model.GraphicalObject;
+
+public class ExtractionMission extends Mission {
+
+    @Override
+    protected void customStep(double progress) {
+
+    }
+}
