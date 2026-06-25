65658ed02b77c6f904ac5b79570bcd5f5217081a
Bunterninja2609 <121108968+Bunterninja2609@users.noreply.github.com>
2026-06-25 16:13:53 +0200

    beginnings of something anew

--- src/main/java/my_project/model/modes/start/StartMode.java
+++ src/main/java/my_project/model/modes/start/StartMode.java
@@ -1,6 +1,10 @@
 package my_project.model.modes.start;
 
+import my_project.control.ModeController;
 import my_project.model.modes.Mode;
 
 public class StartMode extends Mode {
+    public StartMode(ModeController modeController) {
+        super(modeController);
+    }
 }
